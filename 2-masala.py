ac9d81e8c39a5b1ca408918811731c3e8de3b16d
mansurovnan <nozaninorolova2@gmail.com>
2026-07-01 19:18:03 +0500

    done

--- 2-masala.py
+++ 2-masala.py
@@ -1,3 +1,47 @@
+def massiv(func):
+    def wrapper(a):
+        b = []
+        total = 0
+        for i in a:
+            total+=1
+            
+            
+        return func(a)
+    return wrapper
+@massiv
+def r(b):
 
-        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
